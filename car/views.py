0ed75eee4bf96a5060d889a545cc796f0b4d0abe
Faisal-Kushha <fkushha89@hotmail.com>
2021-11-22 21:37:22 +0200

    lab32

--- car/views.py
+++ car/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from .models import Car
+from .serializers import CarSerializer
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from .permissions import IsAuthorOrReadOnly
 
-# Create your views here.
+class CarList(ListCreateAPIView):
+    permission_classes = (IsAuthenticatedOrReadOnly,)
+    queryset = Car.objects.all()
+    serializer_class = CarSerializer
+
+class CarDetail(RetrieveUpdateDestroyAPIView):
+    permission_classes = (IsAuthorOrReadOnly,)
+    queryset = Car.objects.all()
+    serializer_class = CarSerializer
